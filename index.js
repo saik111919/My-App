81a9504fef16feb68905efacd0ee270caadbb381
saik111919 <111496874+saik111919@users.noreply.github.com>
2024-07-08 00:06:36 +0530

    Chanes to check

--- index.js
+++ index.js
@@ -1,76 +1,7 @@
-// const express = require("express");
-// const dotenv = require("dotenv");
-// const mongoose = require("mongoose");
-// // const { getClientAndCollection } = require("./Src/DB");
-// const { getClientAndCollection } = require("./Src/DB/db");
-// const app = express();
-// const cors = require("cors");
-// const path = require("path");
-
-// dotenv.config();
-
-// // Import routes
-
-// const authRoute = require("./Src/Routes/auth");
-
-// // DATABASE
-// getClientAndCollection();
-
-// const db = mongoose.connection;
-
-// db.on("error", (error) => {
-//   console.error("MongoDB connection error:", error);
-// });
-
-// db.once("open", () => {
-//   console.log("Connected to MongoDB");
-// });
-
-// // Optionally, you can also handle the "disconnected" event
-// db.on("disconnected", () => {
-//   console.warn("MongoDB disconnected");
-// });
-
-// process.on("SIGINT", () => {
-//   mongoose.connection.close(() => {
-//     console.log("MongoDB connection closed through app termination");
-//     process.exit(0);
-//   });
-// });
-
-// // Middlewares
-
-// const corsOptions = {
-//   origin: [
-//     "https://urban-space-goldfish-v9w74www7jxfp67q-5173.app.github.dev",
-//     "http://localhost:5173",
-//     "http://localhost:4000",
-//     "http://localhost:5000",
-//     "https://my-app-one-chi-83.vercel.app",
-//     "*",
-//   ],
-//   credentials: true, //access-control-allow-credentials:true
-//   // optionSuccessStatus: 200,
-// };
-// app.use(cors(corsOptions));
-// app.use(express.json());
-
-// app.use("/api", authRoute);
-
-// app.use(express.static("public"));
-
-// // app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html')));
-// app.get("*", (req, res) =>
-//   res.sendFile(path.resolve(__dirname, "public", "index.html"))
-// );
-
-// const PORT = process.env.PORT || 4000;
-// app.listen(PORT, () => {
-//   console.log(`Server is listening on port ${PORT}`);
-// });
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+// const { getClientAndCollection } = require("./Src/DB");
 const { getClientAndCollection } = require("./Src/DB/db");
 const app = express();
 const cors = require("cors");
@@ -79,6 +10,7 @@ const path = require("path");
 dotenv.config();
 
 // Import routes
+
 const authRoute = require("./Src/Routes/auth");
 
 // DATABASE
@@ -94,6 +26,7 @@ db.once("open", () => {
   console.log("Connected to MongoDB");
 });
 
+// Optionally, you can also handle the "disconnected" event
 db.on("disconnected", () => {
   console.warn("MongoDB disconnected");
 });
@@ -106,6 +39,7 @@ process.on("SIGINT", () => {
 });
 
 // Middlewares
+
 const corsOptions = {
   origin: [
     "https://urban-space-goldfish-v9w74www7jxfp67q-5173.app.github.dev",
@@ -113,19 +47,21 @@ const corsOptions = {
     "http://localhost:4000",
     "http://localhost:5000",
     "https://my-app-one-chi-83.vercel.app",
+    "*",
   ],
-  credentials: true,
+  credentials: true, //access-control-allow-credentials:true
+  // optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api", authRoute);
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "public", "index.html"))
+);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
